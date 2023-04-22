cba549f9aafea774c2993bad397457cf24564e7a
Sal Frisina+++++++++++++ <Sfrisina@gmail.com>
2023-04-22 16:30:35 -0400

    all images fixed

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,7 +1,13 @@
+import ecommImg from '../assets/newecomm.jpg'
+import febroseImg from '../assets/febrose.jpg'
+import maggieImg from '../assets/maggiesnew.jpg'
+import patchpupsImg from '../assets/patchpupsscreen.jpg'
+
+
 export default [
     {
         title: 'Full-Stack Ecommerce App',
-        imgUrl: 'src/assets/newecomm.jpg',
+        imgUrl: ecommImg,
         stack: ['React', 'Redux', 'Boostrap', 'Node', 'Express', 'MongoDB'],
         link: 'https://fullstack-ecomm.onrender.com',
         description: 'A Full-Stack E-Commerce app built with React, Node, Express & MongoDB (hosted on Render.com, may take 30seconds to spin-up)',
@@ -9,21 +15,21 @@ export default [
     },
     {
         title: `Maggie's Kitchens & Baths`,
-        imgUrl: 'src/assets/maggiesnew.jpg',
+        imgUrl: maggieImg,
         stack: ['HTML', 'CSS', 'JavaScript', 'PHP' ],
         link: 'http://maggieskitchensandbaths.com',
         description: 'A showroom website I created for a kitchen & bath retailer which shows a selection of products that they offer, and galleries showing work completed.'
     },
     {
         title: 'The Patch Pups',
-        imgUrl: 'src/assets/patchpupsscreen.jpg',
+        imgUrl: patchpupsImg,
         stack: ['HTML', 'CSS', 'JavaScript', 'Tailwind' ],
         link: 'https://thepatchpups.com',
         description: 'A website created for a dog groomer/trainer, which displays services offered and multiple contact forms.'
     },
     {
         title: 'February Rose',
-        imgUrl: 'src/assets/febrose.jpg',
+        imgUrl: febroseImg,
         stack: ['Created with Showit & Shopify' ],
         link: 'https://februaryrose.com',
         description: 'A portfolio/e-commerce store created for a local artist.'
