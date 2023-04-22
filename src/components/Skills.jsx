1fbbd4c7af89cd9ba88c685c3ca38ea449d5b572
Sal Frisina+++++++++++++ <Sfrisina@gmail.com>
2023-04-22 16:06:48 -0400

    added skills icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,6 +6,14 @@ import css3Icon from '../assets/icons/icons8-css3.svg'
 import jsIcon from '../assets/icons/icons8-javascript.svg'
 import sassIcon from '../assets/icons/icons8-sass.svg'
 import tailwindIcon from '../assets/icons/icons8-tailwind-css.svg'
+import nodeIcon from '../assets/icons/icons8-node-js.svg'
+import expressIcon from '../assets/icons/icons8-express-js-49.svg'
+import mongoIcon from '../assets/icons/icons8-mongodb.svg'
+import vscIcon from '../assets/icons/icons8-visual-studio-code-2019.svg'
+import gitIcon from '../assets/icons/icons8-git.svg'
+import githubIcon from '../assets/icons/icons8-github.svg'
+import jestIcon from '../assets/icons/jest-seeklogo.com.svg'
+
 
 const Skills = (props, ref) => {
   return (
@@ -33,12 +41,12 @@ const Skills = (props, ref) => {
                 <h3 className="skill-text">Back-End</h3>
                 <div className='skill-list'>
                 <div>
-                <img className='skill-icon' src='src\assets\icons\icons8-javascript.svg'/>
-                    <img className='skill-icon' src='src\assets\icons\icons8-node-js.svg'/>
+                    <img className='skill-icon' src={jsIcon}/>
+                    <img className='skill-icon' src={nodeIcon}/>
                 </div>
                 <div>
-                    <img className='skill-icon' src='src\assets\icons\icons8-express-js-49.svg'/>
-                    <img className='skill-icon' src='src\assets\icons\icons8-mongodb.svg'/>
+                    <img className='skill-icon' src={expressIcon}/>
+                    <img className='skill-icon' src={mongoIcon}/>
                     </div>
                 </div>
             </div>
@@ -46,12 +54,12 @@ const Skills = (props, ref) => {
             <h3 className="skill-text">Tools</h3>
              <div className='skill-list'>
                 <div>
-                <img className='skill-icon' src='src\assets\icons\icons8-visual-studio-code-2019.svg'/>
-                <img className='skill-icon' src='src\assets\icons\icons8-git.svg'/>
+                <img className='skill-icon' src={vscIcon}/>
+                <img className='skill-icon' src={gitIcon}/>
                 </div>   
                 <div> 
-                    <img className='skill-icon' src='src\assets\icons\icons8-github.svg'/>
-                    <img className='skill-icon' src='src\assets\icons\jest-seeklogo.com.svg'/>
+                    <img className='skill-icon' src={githubIcon}/>
+                    <img className='skill-icon' src={jestIcon}/>
                 </div>
             </div>
             </div>
