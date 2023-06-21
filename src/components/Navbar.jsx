48381217b298c185ee2fad2e0e669d91ba7e1da1
Sal Frisina+++++++++++++ <Sfrisina@gmail.com>
2023-06-20 22:28:13 -0400

    changed resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { forwardRef } from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faBars, faTimes} from '@fortawesome/free-solid-svg-icons'
-import sweResume from '../assets/Salfrisina-SWE0406.pdf'
+import sweResume from '../assets/swe-resume-0620.pdf'
 
 
 
